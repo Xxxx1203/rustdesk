975a4a430c50b4f752f28f9a447361a178ff75ac
x <112153174+Xxxx1203@users.noreply.github.com>
2024-12-10 15:25:47 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,7 +100,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rustdesk6.lki.cc"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rustdesk.lki.cc"];
 pub const PUBLIC_RS_PUB_KEY: &str = "OhRbZSthJNntE7a9Y+Ge3Wf3GZXEEnU8nBMwOze0NjM=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
